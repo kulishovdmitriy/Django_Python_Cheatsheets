49ff335e2ed0dd3920735d40bfafd1b76f226337
dmitriy <kylishovdimka@ukr.net>
2024-10-09 21:33:32 +0300

    added logging and update app source

--- src/source/urls.py
+++ src/source/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from source.views import InformationDetailView, SourceListView, TopicListView
+from source.views import InformationDetailView, SourceListView, TopicListView, InformationCreateView
 
 app_name = "source"
 
 urlpatterns = [
     path("topic/", TopicListView.as_view(), name="topic_list"),
-    path("source/<int:id>/", SourceListView.as_view(), name="source_detail"),
-    path("info/<int:id>/", InformationDetailView.as_view(), name="information_detail"),
+    path("source/<int:id>/", SourceListView.as_view(), name="source_list"),
+    path('information/create/<int:source_id>/', InformationCreateView.as_view(), name='information_create'),
+    path("info/<int:id>/", InformationDetailView.as_view(), name="information_list"),
 ]
